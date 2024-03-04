d4c1e2b2fa6f0ecbfdb31413ef540aba49df8fc9
b12s306 <santiago.guti17correa@gmail.com>
2024-03-04 13:31:49 -0500

    Add work in class

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,8 +1,48 @@
-class Todo:
-    pass
+from typing import List
 
+
+class Todo:
+    def __init__(self,code_id: int, tittle: str, description: str):
+        self.code_id: int = code_id
+        self.tittle: str = tittle
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
+
+    def mark_completed(self):
+        self.completed = True
+        return self.completed
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+        pass
+
+    def __str__(self):
+        return f"{self.code_id}-{self.tittle}"
 
 class TodoBook:
-    pass
+    def __init__(self):
+        self.todos: dict[int, Todo]={}
+
+    def add_todo(self, tittle: str, description: str) -> int:
+        id = len(self.todos)+1
+        todo = Todo(id,tittle, description)
+        self.todos[id] = todo
+        return id
+
+    def pading_todos(self) -> list[int]:
+        list = [t for t in self.todos if self.Todo.completed==0]
+        return list
+
+    def completed_todos(self) -> list[Todo]:
+        list = [t for t in self.todos if self.t.completed == 1]
+        return list
+
+    def tags_todo_count(self) -> dict[str, int]:
+        pass
+
+
+
 
 
